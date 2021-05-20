2cb9583f3f6c20aeeec0d35ec5676352793b4146
Elena Slyusareva <sportgirl55@bk.ru>
2021-05-20 18:05:46 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import propTypes from 'prop-types';
+
+const item2 = {
+  brand: "Абибас",
+  title: "Пальтишко",
+  description: "Синее мужское пальтишко",
+  descriptionFull:
+    "Men's minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.",
+  price: 8,
+  currency: "$",
+};
+
+function ShopItemFunc(props) {
+  const { item } = props;
+  return (
+    <div className="main-content">
+      <h2>{item.brand}</h2>
+      <h1>{item.title}</h1>
+      <h3>{item.description}</h3>
+      <div className="description">{item.descriptionFull}</div>
+      <div className="highlight-window mobile">
+        <div className="highlight-overlay"></div>
+      </div>
+      <div className="divider"></div>
+      <div className="purchase-info">
+        <div className="price">
+          {item.currency} {parseFloat(item.price).toFixed(2)}
+        </div>
+        <button>Добавить в корзину</button>
+      </div>
+    </div>
+  );
+}
+
+ShopItemFunc.propTypes = { item: propTypes.object.isRequired };
+
+function App() {
+  return (
+    <div className="container">
+      <div className="background-element"></div>
+      <div className="highlight-window">
+        <div className="highlight-overlay"></div>
+      </div>
+      <div className="window">
+        <ShopItemFunc item={item2} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
